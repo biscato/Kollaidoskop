e81d8ed9114e9d79bd50c6abcf0d5ab02b76f4d8
GermanyDog <janruessel@gmail.com>
2012-12-05 20:01:16 +0100

    Slider issue fixed, Charts updated, clean up

--- war/TeamHealth/scripts/central.js
+++ war/TeamHealth/scripts/central.js
@@ -5,7 +5,7 @@ $('#vote').live('pagebeforecreate',function(event, ui){
     $.ajax(
         {
             type: "GET",
-            url:  'http://192.168.0.100:8888/questionaire',
+            url:  'http://192.168.0.114:8888/teamhealth/questionaire',
             //url:  'http://localhost:8888/questionaire',
             data: "{}",
             contentType: "application/json; charset=utf-8",
@@ -47,14 +47,34 @@ function displayAllQuestions(targetDOMElement){
         html += "<div>Question # "+questionsCounter+ ": </div>"; //TODO: localize text
         html += "<div><label>" + biscatoQuestions[i].description + "</label></div>"; //TODO: localize text
         html += "<div data-role='fieldcontain'>";
-        html += "<input class='biscatoQuestionSlider' type='range' name='slider-" + i + "' id='slider-" + i + "' value='" + sliderStartValue + "' min='" + sliderMinValue + "' max='" + sliderMaxValue + "' />";
-        html += "<label for='slider-" + i + "'>Value:</label>"; //TODO: localize text
+        //html += "<input class='biscatoQuestionSlider' type='range' name='slider-" + i + "' id='slider-" + i + "' value='" + sliderStartValue + "' min='" + sliderMinValue + "' max='" + sliderMaxValue + "' />";
+        html += "<input type='range' name='slider-" + i + "' id='slider-" + i + "' value='" + sliderStartValue + "' min='" + sliderMinValue + "' max='" + sliderMaxValue + "' />";
+        html += "<label for='slider-" + i + "'></label>"; //TODO: localize text
         html += "</div>";
         html += "<br>";
     }
-    $(html).appendTo($("#voting"));
+    //$(html).appendTo($("#voting"));
+    $("#voting").append(html).trigger('create');
 }
 
 function getQuestions(){
     return questions;
 }
+
+$('#myVotes').live('pageshow',function(event, ui){
+
+    $.jqplot('myVotesChart',  [[[7,5],[8, 4],[9, 5],[10,7],[11,7],[12,8]]],
+        { title:'My Votes',
+            axes:{yaxis:{min:0, max:10}},
+            series:[{color:'#5FAB78'}]
+        });
+});
+
+$('#teamResults').live('pageshow',function(event, ui){
+
+    $.jqplot('teamResultsChart',  [[[7,5],[8, 4],[9, 5],[10,7],[11,7],[12,8]]],
+        { title:'Team Votes',
+            axes:{yaxis:{min:0, max:10}},
+            series:[{color:'#5FAB78'}]
+        });
+});
